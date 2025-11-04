c9e9fd495f16007883f07ead98d8c4922ff3b476
ThiagoMartins2611 <thiagogames2611@gmail.com>
2025-11-04 11:49:07 -0400

    THAIGO --> RESOLUÇÃO DE BUGS
    Co-authored-by: Rafael Debastiani <RafaelDeba@users.noreply.github.com>DELETAR ITEM
    Co-authored-by: Leticiasa26 <Leticiasa26@users.noreply.github.com> CSS ARRUMADO E MESAGEMS BONITAS

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
-
 const api = axios.create ( {
 
     baseURL:import.meta.env.VITE_API_URL
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,128 +1,155 @@
-import SiteFooter from "../siteDivision/SiteFooter.tsx"
-import SiteHeader from "../siteDivision/SiteHeader.tsx"
-import api from "../../api/api.ts"
-import { useEffect } from "react"
-import { useState } from "react"
-import '../../cssComponents/pages/HomePage.css'
-import pencil from '../../images/pencil.svg'
-import trash from '../../images/trash.svg'
-import {jwtDecode} from 'jwt-decode'
+import SiteFooter from "../siteDivision/SiteFooter.tsx";
+import SiteHeader from "../siteDivision/SiteHeader.tsx";
+import api from "../../api/api.ts";
+import { useEffect, useState } from "react";
+import "../../cssComponents/pages/HomePage.css";
+import pencil from "../../images/pencil.svg";
+import trash from "../../images/trash.svg";
+import { jwtDecode } from "jwt-decode";
 
 interface Item {
-    id: number;
-    name: string;
-    description: string;
-    price: number;
-    imageUrl: string;
-    userId: number;
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  userId: number;
 }
+
 interface Usuario {
-
-  _id:string,
-  name:string,
-  age:number,
-  email:string,
-  key:string,
-  adm:boolean
-  
+  _id: string;
+  name: string;
+  age: number;
+  email: string;
+  key: string;
+  adm: boolean;
 }
 
-function HomePage(){
+function HomePage() {
+  const [mensagem, setMensagem] = useState("");
+  const [error, setError] = useState(false);
+  const [items, setItems] = useState<Item[]>([]);
+  const [usuario, setUsuario] = useState<Usuario>({
+    _id: "",
+    name: "",
+    age: 0,
+    email: "",
+    key: "",
+    adm: false,
+  });
 
-    const [mensagem, setMensagem] = useState("")
-    const [error, setError] = useState(false)
-    const [items, setItems] = useState<Item[]>([])
-      const [ usuario, setUsuario ] = useState <Usuario> ( {
+  const [activePopUp, setActivePopUp] = useState(false);
+  const [itemParaApagar, setItemParaApagar] = useState<string | null>(null);
+
+  function confirmar(itemId: string) {
+    console.log(itemId)
+    setItemParaApagar(itemId);
+    setActivePopUp(true);
+  }
+
+  function naoApagar() {
+    setActivePopUp(false);
+    setItemParaApagar(null);
+  }
+
+  async function apagarItem(itemId: any) {
+    try {
+        console.log(itemId)
+      const DeletedItemId = itemId;
+      const res = await api.post("/private/DeleteItem", {DeletedItemId});
+
+      if (res.status === 200) {
+        console.log("Item apagado com sucesso");
     
-        _id:"",
-        name:"",
-        age:0,
-        email:"",
-        key:"",
-        adm:false
-        
-    } );
+        setItems((prevItems) => prevItems.filter((item) => item._id !== itemId));
+        setActivePopUp(false);
+        setItemParaApagar(null);
+      } else {
+        alert("Erro ao apagar o item. Tente novamente.");
+      }
+    } catch (error) {
+      console.error("Erro ao apagar item:", error);
+      alert("Item não foi apagado. Ocorreu um erro inesperado.");
+    }
+  }
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
 
+    if (token) {
+      const decodedPayload = jwtDecode<Usuario>(token);
+      setUsuario(decodedPayload);
+    }
 
-    useEffect(() => {
+    async function puxarItens() {
+      try {
+        const res = await api.get("/public/ShowItems");
+        setMensagem("Itens carregados com sucesso!");
+        setItems(res.data);
+        setError(false);
+      } catch (error) {
+        console.error(error);
+        setMensagem("Erro ao carregar os itens.");
+        setError(true);
+      }
+    }
 
+    puxarItens();
+  }, []);
 
-        const token = localStorage.getItem ( "token" );
+  return (
+    <>
+      <SiteHeader />
 
-        if ( token ) {
-        
-        const decodedPayload = jwtDecode <Usuario> ( token )
-        
-            setUsuario ( decodedPayload )
-        }
+      <main id="HomePage">
 
+        <div className="items-container">
+          {error && <div className="error-message">{mensagem}</div>}
 
-        async function puxarItens() {
-            try {
-                const res = await api.get("/public/ShowItems");
+          {items.map((item) => (
+            <div key={item._id} className="item-card">
+              <img src={item.imageUrl} alt={item.name} className="item-image" />
+              <h2>{item.name}</h2>
+              <h1>{item.price}</h1>
 
-                setMensagem ( "Itens carregados com sucesso!" );
-                setItems(res.data);
-                setError(false);
+              {usuario.adm && (
+                <div id="editarEapagar">
+                  <img src={pencil} alt="editar" id="editar" />
+                  <img
+                    onClick={() => {
+                        confirmar(item._id)
+                        console.log(item._id)
+                    }}
+                    src={trash}
+                    alt="apagar"
+                    id="apagar"
+                  />
+                </div>
+              )}
+            </div>
+          ))
+          
+          }
+        </div>
 
-            } catch (error) {
+        {activePopUp && (
+          <div id="areaConfirm">
+            <div id="confirm">
+              <h2>Gostaria mesmo de apagar esse item?</h2>
+              <button
+                onClick={() => apagarItem(itemParaApagar)}
+              >
+                Sim
+              </button>
+              <button onClick={naoApagar}>Não</button>
+            </div>
+          </div>
+        )}
+      </main>
 
-                setMensagem ( "Erro ao carregar os itens." );
-                setError(true);
-            }
-      
-        }
-
-        puxarItens();
-
-    }, [])
-
-    return(<>
-
-        <SiteHeader/>
-
-            <main id="HomePage">
-
-                <div className="items-container">
-                    {error && (
-                        <div className="error-message">{mensagem}</div>
-                    )
-
-                    }
-                {
-                     items.map((item) => {
- // CORRIGIDO: Removido o fragmento <>...</>
-                        return (
-                        // A key está agora no elemento raiz retornado: o div
-                                <div key={item.id} className="item-card">
-                                    <img src={item.imageUrl} alt={item.name} className="item-image" />
-                                    <h2>{item.name}</h2>
-                                    <h1>{item.price}</h1>
-
-                                    {usuario.adm && (
-
-                                        <div id='editarEapagar'>
-                                            <img onClick={editarItem} src={pencil} alt="" id="editar"/>
-                                            <img src={trash} alt="" id="apagar"/>
-                                            <img onClick={apagarItem} src={trash} alt="" id="editar"/>
-                                        </div>
-                                    )
-                                    
-                                    }
-                               
-
-                                </div>
-                            )
-                            })
-                        }
-                </div>      
-
-            </main>
-      
-        <SiteFooter/>
-    </>)
-
+      <SiteFooter />
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;

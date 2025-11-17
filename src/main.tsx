889da2c1a65f3165c2064183c3cc74aca756d644
Thiago Costa Martins <thiagogames2611@gmail.com>
2025-11-17 00:34:02 -0300

    redirecionamnete da rota vazia

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -7,12 +7,21 @@ import LoginPage from './components/pages/LoginPage'
 import CadastrarItensPage from './components/pages/CadastrarItensPage'
 import CarrinhoPage from './components/pages/CarrinhoPage'
 import Carrinhos from './components/pages/CarrinhosADM'
+import { useNavigate } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <BrowserRouter>
     <Routes>
 
+
+        <Route path='/' element={<>{()=>{
+          const navigate = useNavigate()
+          useEffect(()=>{
+            navigate("/HomePage");
+          }, [])
+
+        }}</>}/>
         <Route path='/HomePage' element={<HomePage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/cadastrarItem' element={<CadastrarItensPage/>}/>
